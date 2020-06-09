03a63a010cd26f604f5d735c894146aa24214a81
azinum <lukaska61@gmail.com>
2020-06-09 16:47:14 +0200

    Added hold on notes

--- src/instrument.c
+++ src/instrument.c
@@ -22,16 +22,18 @@ struct Note_info {
   float amp;
   float release_speed;
   float attack_speed;
+  int hold_time;
+  int time;
   int state;
 };
 
 static int index = 0;
 static int tempo = 40;
 static struct Note_info seq_table[] = {
-  {0, 0, 0.00001f, 0.001f, STATE_NONE},
-  {0, 0, 0.00001f, 0.001f, STATE_NONE},
-  {0, 0, 0.00001f, 0.001f, STATE_NONE},
-  {0, 0, 0.00001f, 0.001f, STATE_NONE},
+  {0, 0, 0.00001f, 0.05f, 5000, 0, STATE_NONE},
+  {0, 0, 0.00001f, 0.05f, 5000, 0, STATE_NONE},
+  {0, 0, 0.00001f, 0.05f, 5000, 0, STATE_NONE},
+  {0, 0, 0.00001f, 0.05f, 5000, 0, STATE_NONE},
 };
 
 static float amp_max = 0.2f;
@@ -48,17 +50,24 @@ float instrument_process() {
   struct Note_info* current_note = &seq_table[index];
   if (!(engine_time % (tempo * FRAMES_PER_BUFFER))) {
     index = (index + 1) % (ARR_SIZE(seq_table));
-    // current_note->state = STATE_ATTACK;
-    current_note->state = STATE_RELEASE;
-    current_note->amp = amp_max;
+    current_note->state = STATE_ATTACK;
+    current_note->amp = 0;
   }
   for (int i = 0; i < (int)ARR_SIZE(seq_table); i++) {
     struct Note_info* note = &seq_table[i];
     switch (note->state) {
       case STATE_ATTACK: {
+        note->amp += note->attack_speed;
+        if (note->amp > amp_max) {
+          note->amp = amp_max;
+          note->state = STATE_HOLD;
+          note->time = engine_time;
+        }
         break;
       }
       case STATE_HOLD: {
+        if (note->time + note->hold_time < engine_time)
+          note->state = STATE_RELEASE;
         break;
       }
       case STATE_RELEASE: {
